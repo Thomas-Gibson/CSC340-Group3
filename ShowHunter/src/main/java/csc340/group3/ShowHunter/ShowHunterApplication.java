3fc004afac4124c267fcc22b31daeb373d29da82
Thomas-Gibson <jtgibson@uncg.edu>
2024-11-05 09:00:10 -0500

    Added springboot setup for restAPI

---
+++ ShowHunter/src/main/java/csc340/group3/ShowHunter/ShowHunterApplication.java
@@ -0,0 +1,13 @@
+package csc340.group3.ShowHunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowHunterApplication.class, args);
+	}
+
+}
---
+++ ShowHunter/src/test/java/csc340/group3/ShowHunter/ShowHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package csc340.group3.ShowHunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowHunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
